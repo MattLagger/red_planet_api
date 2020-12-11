d23c09e594ff5f0e02d98f39d794b7d829982874
Wendreo Matheus <wendreo.pvh@gmail.com>
2020-12-11 10:53:45 -0300

    Add Listagem de Imagens Com Paginação

--- app/controllers/rovers_controller.rb
+++ app/controllers/rovers_controller.rb
@@ -1,7 +1,21 @@
 class RoversController < ApplicationController
   def index
     rovers = NasaRovers.new
-    rovers_list = rovers.all
+    rovers_list = rovers.all_rovers
+
+    render json: rovers_list
+  end
+
+  def manifests
+    rovers = NasaRovers.new
+    rovers_list = rovers.manifests(params[:rover])
+
+    render json: rovers_list
+  end
+
+  def photos
+    rovers = NasaRovers.new
+    rovers_list = rovers.photos(params)
 
     render json: rovers_list
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # Rotas API NASA
   get '/rovers', to: 'rovers#index'
+  get '/manifests/:rover', to: 'rovers#manifests'
+  get '/photos', to: 'rovers#photos'
+
   get '/auth/github', to: 'authentication#github', format: false
-  
+
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
