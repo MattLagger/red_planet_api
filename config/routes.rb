c96c1c49ccc46710f631b1359221b438f6df03a9
Wendreo Matheus <wendreo.pvh@gmail.com>
2021-01-06 09:48:55 -0400

    Fix Ajuste nas rotas de favoritos

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :favorites
   # Rotas API NASA
   get '/rovers', to: 'rovers#index'
   get '/manifests/:rover', to: 'rovers#manifests'
   post '/photos', to: 'rovers#photos'
 
+  # Rotas Autenticacao Usuário via Github
   get '/auth/github', to: 'authentication#github', format: false
+
+  # Rotas Favoritos
+  scope 'favorites' do
+    get '/', to: 'favorites#index'
+    post '/', to: 'favorites#create'
+    delete '/:id', to: 'favorites#destroy'
+  end
 
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
